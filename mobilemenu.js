73d1f6b95726ec4c512d71bbc6787c3a3482e113
Maya Mircheva <may4eto@hotmail.com>
2021-01-28 00:39:16 +0100

    fixed page  overflowon mobile

--- mobilemenu.js
+++ mobilemenu.js
@@ -2,3 +2,8 @@ $(".menu-toggle").on("click", function() {
     $(".mobile-menu-wrapper").toggleClass("d-none").toggleClass("d-flex")
     return false
   })
+
+  $(".menu-toggle").on("click", function() {
+    $("body").toggleClass("hidden")
+    return false
+  })
